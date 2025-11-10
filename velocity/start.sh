5c8c44226710b006d04815847773b128455fccc3
Mario-1981 <guenther.mario@googlemail.com>
2025-11-10 10:26:59 +0100

    bug fixing

--- velocity/start.sh
+++ velocity/start.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 set -e
 
-echo "Starting Velocity proxy with:"
+# Init: Prüfen ob gemountetes Verzeichnis existiert und JAR fehlt
+if [ ! -f /velocity/velocity.jar ]; then
+    echo "Kopiere Standarddateien ins gemountete Verzeichnis..."
+    cp /velocity-default/* /velocity/
+fi
+
+# Server starten
+echo "Starting Velocity proxy..."
 echo "  Xms = $JAVA_XMS"
 echo "  Xmx = $JAVA_XMX"
 echo "  Options = $JAVA_OPTS"
 
-exec java -Xms${JAVA_XMS} -Xmx${JAVA_XMX} $JAVA_OPTS -jar velocity.jar
+exec java -Xms${JAVA_XMS} -Xmx${JAVA_XMX} $JAVA_OPTS -jar /velocity/velocity.jar
